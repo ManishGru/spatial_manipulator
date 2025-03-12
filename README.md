c76402bc2a55a192e5fdfc8af667f87d9cfb7d00
ManishGru <gurumanish2015@gmail.com>
2025-03-12 16:15:52 +0545

    added ros2 control for differential drive bot and improved readme

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# spatial_manipulator
+# Spatial Manipulator
+
+## Setting up package
+Change the directory to your workspace
+
+    cd <PATH_TO_WS>/src
+
+Clone this repo in the location
+
+    git clone https://github.com/ManishGru/spatial_manipulator
+
+## Setting up Gazebo world
+
+Clone below repo with models and worlds. 
+
+    git clone https://github.com/leonhartyao/gazebo_models_worlds_collection
+
+Add the following line in .bashrc for loading model and worlds in gazebo
+
+    source /usr/share/gazebo/setup.bash
+
+    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:<PATH>/gazebo_models_worlds_collection/models
+    
+    export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:<PATH>/gazebo_models_worlds_collection/worlds
+
+## Building 
+
+Change the directory to your workspace
+
+    cd <PATH_TO_WS>
+
+Build the package using colcon build command
+
+    colcon build --symlink-install --packages-select spatial_manipulator
+
+## Launching Gazebo Sim and Rviz
+
+Differential Drive control can be setup in two different ways either using ros2_control or gazebo_plugin. For launching Gazebo simulation and rviz with bot using ros2_control:
+
+    ros2 launch spatial_manipulator robot.launch.py use_ros2_control:=true
+
+Using Gazebo Plugin:
+
+     ros2 launch spatial_manipulator robot.launch.py use_ros2_control:=false
+
+
+
+## Controlling the Bot
+
+Bot can be controlled using teleop_twist_keyboard node.
+For ros2_control: 
+
+    ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstammped
+
+
+For Gazebo plugin: 
+
+    ros2 run teleop_twist_keyboard teleop_twist_keyboard
--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -2,7 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -12,12 +12,14 @@ import xacro
 
 def generate_launch_description():
     
+    use_ros2_control= LaunchConfiguration("use_ros2_control")
+    
     pkg_path = os.path.join(get_package_share_directory('spatial_manipulator'))
     xacro_file = os.path.join(pkg_path,"description","robot.urdf.xacro")
-    robot_description_config = xacro.process_file(xacro_file)
-    
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(['xacro ', xacro_file,' use_ros2_control:=',use_ros2_control])
     params = {
-        'robot_description':robot_description_config.toxml(),
+        'robot_description':robot_description_config,
         'use_sim_time':True,
     }
     
@@ -28,10 +30,12 @@ def generate_launch_description():
         parameters=[params]
     )
     
+    gazebo_params_file = os.path.join(pkg_path,"config","gazebo_params.yaml")
     
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("gazebo_ros"),"launch","gazebo.launch.py")]),
-        launch_arguments={"world":os.path.join(pkg_path,"world","office_small.world")}.items()
+        launch_arguments={"world":os.path.join(pkg_path,"world","office_small.world"),
+                          'extra_gazebo_args':'--ros-args --params-file' + gazebo_params_file}.items()
     ) 
     spawn_entity = Node(
         package="gazebo_ros",
@@ -41,8 +45,22 @@ def generate_launch_description():
             "robot_description",
             "-entity",
             "spatial_manipulator_bot",
+            "-timeout",
+            "120"
         ],
         output='screen',
+    )
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"]
+    )
+    
+    joint_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"]
     )
     
     rviz2 = Node(
@@ -55,17 +73,15 @@ def generate_launch_description():
         ]
     )
     
-    joint_state_publisher_gui= Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-        output="screen"
-    )
-    
-    
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_ros2_control",
+            default_value="true",
+            description="Use ros2 control if true"),
         node_robot_state_publisher,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broadcaster_spawner,
         rviz2,
-        # joint_state_publisher_gui
     ])
